d07e61fccb47c019b210396fb6ba3b9184bd4813
Kevin Nolasco <kevnolasco95@gmail.com>
2026-02-05 21:09:28 -0800

    updating y lims

--- accountability_dashboard.py
+++ accountability_dashboard.py
@@ -293,7 +293,7 @@ for i, (raw, rolling) in enumerate(paired_plots):
         ),
         row=row, col=col
     )
-fig.update_yaxes(range=[1700, 3200], row=3, col=2)
+fig.update_yaxes(range=[1900, 3000], row=3, col=2)
 
 
 # 7-day rolling weight change (BAR)
